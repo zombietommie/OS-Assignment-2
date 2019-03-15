b858bb5dd2dc7f08deaa064e6fa20a8750d334cf
zombietommie <zombietommie@gmail.com>
2019-03-15 09:01:04 -0500

    Worked on vim and terminal update/upgrades to make coding easier.

--- textReader.py
+++ textReader.py
@@ -1,35 +1,36 @@
 # Tommy Tran 2420005 
 # OS Assignment 2 
 # Text Reader without threads
+# textReader.py
 
 # importing the regular expression package
 import re
 
 
 def main():
-    # Select a file to only read
+    # Selected a file to be read ONLY assigning it a variable 'f'
     f = open("enwik9", "r")
     
+    for i in len(f.readline())
+
+
     # reads and places the doc into a var 
     theFile = f.readline()
 
     # sub the line for special character test
-    x = re.sub("[\'\"+=*.<>\/[]|(){}:]", " ",theFile)  # works
+    x = re.sub("[\'\"+=*.<>\/[]|(){}:]", " ",theFile)  
 
     # split the words up into individuals
-    words = x.split()
+    words = x.split() 
 
-    # testing purpose to print out the one line and print the len of the list
-    print(words)
-    print(len(words))
-    
-    # test print what's in words
-    print(words[1])  # this will print one word from the list 
-
-    # test print len()
-    print(len(words[1])) #this will print the length of the char in the word (which is index 1)
+    # this will print one word from the list 
+    print(words[1])    
+    #this will print the length of the char in the word (which is index 1)
+    print(len(words[1]))
 
     # Always close after open
     f.close()
 
-main()      # this reads one line of text and splits them up in a list
+
+# Run the methods (def)s 
+main()
