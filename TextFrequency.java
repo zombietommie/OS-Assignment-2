59a53e99c936d2c0909f25b84abd7fb3625625a8
zombietommie <zombietommie@gmail.com>
2019-03-19 21:30:47 -0500

    modified java file to read and print text file

--- TextFrequency.java
+++ TextFrequency.java
@@ -8,8 +8,24 @@
  *
  */ 
 
+// imports
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TextFrequency {
     public static void main(String [] args) {
-
+        try {
+            File textFile = new File("enwik9");
+            Scanner reader = new Scanner(textFile);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error has occurred!");
+            e.printStackTrace();
+        }
     }
 }
